5715061377fc726c3f0545152eae5fa54c49c511
Blake Laprade <lapradeblake@gmail.com>
2024-01-22 17:43:33 -0700

    added other enemy classes
    also made enemies turn on an interval

--- Assets/Scripts/AI/Enemy.cs
+++ Assets/Scripts/AI/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] protected float _speed;
+    [SerializeField] protected float _turnTimer;
+    private float _turnCount = 0;
     private Rigidbody2D _rb;
     private Vector2 _direction = Vector2.right;
 
@@ -14,31 +16,41 @@ public class Enemy : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    protected virtual void Update()
-    {
+    protected virtual void FixedUpdate()
+    { 
+        
+        _turnCount += Time.deltaTime; //timer
+        if( _turnCount >= _turnTimer )
+        {
+            Turn();
+            _turnCount = 0;
+        }
+
         MoveEnemy();
     }
 
     private void MoveEnemy()//moves the  enemy horizontally
-    {
-        //_rb.velocity = _direction * _speed;
-
+    {  
         gameObject.transform.Translate(_direction * _speed * Time.deltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)//turns around when hitting wall
     {
-        if(collision.gameObject.layer != LayerMask.NameToLayer("Ground"))
-        { 
-         _direction *= new Vector2(-1, 0);
-         this.gameObject.transform.localScale *= new Vector2(-1, 1);
+        if(collision.gameObject.layer != LayerMask.NameToLayer("Ground")) //this wont work well depending on how levels are built. will likely have to change
+        {
+            Turn();
         }
         
     }
     protected virtual void Die()//destroys object on death
     {
         Destroy(this.gameObject);
+    }
+
+    private void Turn() //changes enemies direction
+    {
+        _direction *= new Vector2(-1, 0);
+        this.gameObject.transform.localScale *= new Vector2(-1, 1);
     }
 
 
---
+++ Assets/Scripts/AI/ShieldEnemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldEnemy : Enemy
+{
+    // Update is called once per frame
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
