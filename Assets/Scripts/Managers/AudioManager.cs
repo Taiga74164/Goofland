6faf11aefe3b28cb789f4e23d57b1144f1b5b498
Blake Laprade <lapradeblake@gmail.com>
2024-01-28 13:31:36 -0700

    added main theme

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -4,6 +4,7 @@ namespace Managers
 {
     public class AudioManager : Singleton<AudioManager>
     {
+        [SerializeField] private AudioSource _mainTheme;
         public void PlayAudio(object data)
         {
             if (data is AudioSource audioSource)
