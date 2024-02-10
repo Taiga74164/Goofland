924aef097ea1e9100a712643aee0d171ce737f18
Blake Laprade <lapradeblake@gmail.com>
2024-02-10 10:51:35 -0700

    OnOff fixes
    Targets are now synced if more than one are used
    blocks will now become transparent if they are inactive

--- Assets/Scripts/Levels/OnOffBlock.cs
+++ Assets/Scripts/Levels/OnOffBlock.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Levels
@@ -10,12 +11,18 @@ namespace Levels
         [SerializeField] private OnBlock _blockType;
 
         private BoxCollider2D _boxCollider;
+        private SpriteRenderer _sprite;
 
         private void Awake()
         {
             _boxCollider = GetComponent<BoxCollider2D>();
+            _sprite = GetComponent<SpriteRenderer>();
         }
 
+        /// <summary>
+        /// makes this block active or inactive depending on the color of the target and this objects color.
+        /// </summary>
+        /// <param name="data"></param>
         public void Switch(object data)
         {
             if(data is OnBlock onType)
@@ -23,10 +30,18 @@ namespace Levels
                 if (onType == _blockType)
                 {
                     _boxCollider.enabled = true;
+                    //hate this. will change it once we get art for these
+                    Color color = _sprite.color;
+                    color.a = 1f;
+                    _sprite.color = color;
                 }
                 else
                 {
                     _boxCollider.enabled = false;
+                    //hate this. will change it once we get art for these
+                    Color color = _sprite.color;
+                    color.a = .45f;
+                    _sprite.color = color;
                     Debug.Log(onType);
                 }
             }
--- Assets/Scripts/Levels/OnOffTrigger.cs
+++ Assets/Scripts/Levels/OnOffTrigger.cs
@@ -12,6 +12,34 @@ namespace Levels
         // The color that will start active when the scene is loaded  
         [SerializeField] private OnBlock _activeColor;
 
+        private SpriteRenderer _sprite; 
+        
+        public OnBlock ActiveColor 
+        { 
+            get 
+            { 
+                return _activeColor; 
+            } 
+            set //changes sprite color when block type is changed. likely temporary until art assets are recieved for it
+            { 
+                _activeColor = value;
+                if(value == OnBlock.Red)
+                {
+                    _sprite.color = Color.red;
+                }
+                else
+                {
+                    _sprite.color = Color.blue;
+                }
+            }
+        }
+
+        private void Awake()
+        {
+            _sprite = GetComponent<SpriteRenderer>();
+            ActiveColor = ActiveColor;
+        }
+
         private void Start()
         {
             Trigger();
@@ -19,15 +47,21 @@ namespace Levels
 
         public void Trigger() 
         {
-            if(_activeColor == OnBlock.Red)
+            if(ActiveColor == OnBlock.Red)
             {
-                _activeColor = OnBlock.Blue;
+                ActiveColor = OnBlock.Blue;
             }
             else
             {
-                _activeColor = OnBlock.Red;
+                ActiveColor = OnBlock.Red;
             }
-            onTrigger.Raise(_activeColor);
+            onTrigger.Raise(ActiveColor);
+        }
+
+        public void OnTargetEvent(object data)
+        {
+            if(data is OnBlock blockType)
+                ActiveColor = blockType;
         }
     }   
 
