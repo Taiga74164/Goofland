f754363efda4dc75277dcefa95a30f8dc00ab188
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2024-02-21 22:55:54 -0700

    (feat) Added Enemies defeated counter

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -8,12 +8,14 @@ namespace UI
     public class HUD : MonoBehaviour
     {
         public TMP_Text coinText;
+        public TMP_Text defeatedText;
         public TMP_Text timeText;
         private float _timer;
 
         private void Update()
         {
             coinText.text = $"{CurrencyManager.Instance.Currency}";
+            defeatedText.text = $"Defeated: {EntityManager.Instance.defeatedEnemies.Count}";
         }
         
         private void FixedUpdate()
