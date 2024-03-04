7814d01a45c7db9cf759fa2331f46c230d16ef44
Joaquin <67109235+Taiga74164@users.noreply.github.com>
2024-03-03 19:25:29 -0700

    Added ground check for Hippopottymas

--- Assets/Scripts/Enemies/Hippopottymas.cs
+++ Assets/Scripts/Enemies/Hippopottymas.cs
@@ -13,6 +13,7 @@ namespace Enemies
         public GameEvent onDisturbedEvent;
         
         private bool _isDisturbed;
+        private bool _canCharge;
         
         protected override void Update()
         {
@@ -23,8 +24,34 @@ namespace Enemies
                 onDisturbedEvent.Raise(gameObject);
                 _isDisturbed = true;
             }
-            
+
+            if (!_isDisturbed) return;
+            UpdateChargeCondition();
             Charge();
+        }
+        
+        private void UpdateChargeCondition()
+        {
+            // Calculate direction to player.
+            var playerPosition = playerTransform.position;
+            var directionToPlayer = (playerPosition - transform.position).normalized;
+    
+            // Check if the player is to the left or right of the enemy.
+            var isPlayerLeft = directionToPlayer.x < 0;
+            var checkDirection = isPlayerLeft ? Vector2.left : Vector2.right;
+
+            // Perform ground and wall checks in the direction towards the player.
+            var groundPosition = groundDetection.position;
+            var groundForward = groundPosition.Add(checkDirection * rayLength);
+            var down = new Vector2(0, -0.5f);
+            var groundInfo = Physics2D.Raycast(groundForward, down, rayLength, groundLayer);
+            var wallInfo = Physics2D.Raycast(groundPosition, checkDirection, rayLength, turnLayer);
+
+            Debug.DrawLine(groundForward, groundForward.Add(down * rayLength), Color.yellow);
+            Debug.DrawLine(groundPosition, groundPosition.Add(checkDirection * rayLength), Color.red);
+    
+            // Update charge condition based on ground and wall checks.
+            _canCharge = groundInfo.collider && !wallInfo.collider;
         }
         
         public void OnDisturbed(object data)
@@ -40,15 +67,16 @@ namespace Enemies
 
         private void Charge()
         {
-            if (!_isDisturbed) return;
+            if (!_canCharge) return;
             
             // Get player position.
             var playerPosition = playerTransform.position;
             // Get direction towards player.
             var directionToPlayer = (playerPosition - transform.position).normalized;
+            directionToPlayer.y = 0;
             
             // Move towards the player.
-            rb.velocity = new Vector2(directionToPlayer.x * chargeSpeed, rb.velocity.y);
+            transform.Translate(directionToPlayer * (chargeSpeed * Time.deltaTime));
             // Flip the model based on the direction to the player.
             Flip(directionToPlayer.x);
         }
@@ -64,10 +92,6 @@ namespace Enemies
         }
         
         protected override void Patrol()
-        {
-        }
-        
-        protected override void Turn()
         {
         }
         
